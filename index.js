233e70fa2bc42a58f04ae57be04380c9b6a3aa02
kschipulring <karl@3ringprototype.com>
2018-10-17 21:22:54 -0400

    curl experiment 1

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const curl = require("curl");
+const jsdom = require("jsdom");
+
+
+const url = "https://newyork.craigslist.org/search/bka";
+curl.get(url, null, (err,resp,body)=>{
+  if(resp.statusCode == 200){
+     console.log(body);
+  }
+  else{
+     //some error handling
+     console.log("error while fetching url");
+  }
+});
+
 exports.handler = async (event) => {
     // TODO implement
     const response = {
